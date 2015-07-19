7288a292694677358b7830b60dee15f67362ffc8
Abouzar Parvan <abouzar.parvan@gmail.com>
2015-07-19 16:12:59 +0430

    visiting process in finnished

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ from modules.Visitor import Visitor
 
 
 def main():
+
     logging.config.fileConfig('logging.ini')
     logger = logging.getLogger()
-    url = input("please enter URL:")
-    visit = Visitor(url=url, logger=logger)
-    visit.visitNoUI(count=20, timeout=1)
+    try:
+        url = input("please enter URL:")
+        count = int(input("please enter number of visits:"))
+        robot = Visitor(url=url, logger=logger)
+        vistedTimes = robot.visitNoUI(count=count, timeout=1)
+        print("you've asked %d times and i've visited %d times" % (count, robot.validVisits))
+    except Exception as err:
+        logger.error(err)
 
 if __name__ == "__main__":
     main()
--- modules/Visitor.py
+++ modules/Visitor.py
@@ -4,7 +4,7 @@ import requests
 from requests.exceptions import *
 from selenium import webdriver
 
-from tqdm import tqdm
+from modules.tqdm import tqdm
 
 
 class Visitor(object):
@@ -13,6 +13,8 @@ class Visitor(object):
         self.__url = url
         self.logger = logger or logging.getLogger(__name__)
         self.req = requests
+        self.__timeout = 1
+        self.__readCount = 0
         #self.__driver = webdriver.Chrome()
 
     @property
@@ -21,17 +23,52 @@ class Visitor(object):
 
     @url.setter
     def url(self, value):
+        if value != self.url:
+            self.logger.debug("url changed to %s" % (value))
+            self.__readCount = 0
+            self.__timeout = 1
         self.__url = value
 
-    def visitNoUI(self, count=1, timeout=0.1):
+    @property
+    def timeout(self):
+        return self.__timeout
+
+    @timeout.setter
+    def timeout(self, value):
+        self.logger.debug("timeout changed to %d" % (int(value)))
+        self.__timeout = value
+
+    @property
+    def validVisits(self):
+        return int(self.__readCount)
+
+    @validVisits.setter
+    def validVisits(self, value):
+        self.__readCount = value
+
+    def visitNoUI(self, count=1, timeout=None, selfHeal=None):
+        if timeout is not None:
+            self.timeout = timeout
         try:
             for i in tqdm(range(count)):
                 try:
-                    self.req.get(self.url, timeout=timeout)
+                    self.req.get(self.url, timeout=self.timeout)
+                    self.validVisits = self.validVisits + 1
                 except MissingSchema as err:
                     self.logger.err(err)
                     raise RuntimeError("Invalid URL")
                 except ConnectionError as err:
-                    self.logger.info(err)
+                    self.logger.debug(err)
+                except ReadTimeoutError as err:
+                    self.logger.debug(err)
+                    self.timeout = self.timeout + 1
+                except ConnectTimeoutError as err:
+                    self.logger.debug(err)
+                    self.timeout = self.timeout + 1
+            if selfHeal and self.validVisits != count:
+                self.visitNoUI(count=(count - self.validVisits), timeout=self.timeout, selfHeal=False)
+            self.logger.info("visited %s \t {%d} times" % (str(self.url), int(self.validVisits)))
+            return self.validVisits
         except Exception as err:
             self.logger.error(err, exc_info=True)
+            return self.validVisits
