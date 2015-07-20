362dcb396fdbeecbe39126388816455fc19f9ace
Abouzar Parvan <abouzar.parvan@gmail.com>
2015-07-20 13:58:57 +0430

    added tor capability

--- main.py
+++ main.py
@@ -8,14 +8,36 @@ def main():
 
     logging.config.fileConfig('logging.ini')
     logger = logging.getLogger()
-    try:
-        url = input("please enter URL:")
-        count = int(input("please enter number of visits:"))
-        robot = Visitor(url=url, logger=logger)
-        vistedTimes = robot.visitNoUI(count=count, timeout=1)
-        print("you've asked %d times and i've visited %d times" % (count, robot.validVisits))
-    except Exception as err:
-        logger.error(err)
+    while True:
+        try:
+            use_tor = input('Do you want to use MultiIP capability[N]?: ')
+            if use_tor.lower() in ['y', 'yes']:
+                use_tor = True
+            else:
+                use_tor = False
+            url = input("please enter URL:")
+            count = int(input("please enter number of visits:"))
+            robot = Visitor(url=url, logger=logger)
+            vistedTimes = robot.visitNoUI(count=count, timeout=1, selfHeal=True, use_tor=use_tor)
+            print("you've asked %d times and i've visited %d times" % (count, robot.validVisits))
+            result = input("do you want to visit another site[N]")
+            if result.lower() != 'y':
+                print("bye bye...")
+                break
+        except KeyboardInterrupt as err:
+            answer = input("Are you sure you want to exit the program?[Y]")
+            if answer is None:
+                print("Sayonara...")
+                break
+            if answer.lower in ['n', 'no']:
+                continue
+            else:
+                print("Farewell...")
+                break
+        except Exception as err:
+            logger.error(err)
+            continue
+
 
 if __name__ == "__main__":
     main()
--- modules/Visitor.py
+++ modules/Visitor.py
@@ -1,10 +1,14 @@
 import logging
+import socket
+import urllib
 
 import requests
 from requests.exceptions import *
 from selenium import webdriver
 
+import socks
 from modules.tqdm import tqdm
+from sockshandler import SocksiPyHandler
 
 
 class Visitor(object):
@@ -15,6 +19,7 @@ class Visitor(object):
         self.req = requests
         self.__timeout = 1
         self.__readCount = 0
+        self.__tor = urllib.request.build_opener(SocksiPyHandler(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9150))
         #self.__driver = webdriver.Chrome()
 
     @property
@@ -39,6 +44,10 @@ class Visitor(object):
         self.__timeout = value
 
     @property
+    def torBrowser(self):
+        return self.__tor
+
+    @property
     def validVisits(self):
         return int(self.__readCount)
 
@@ -46,27 +55,47 @@ class Visitor(object):
     def validVisits(self, value):
         self.__readCount = value
 
-    def visitNoUI(self, count=1, timeout=None, selfHeal=None):
+    def visitNoUI(self, count=1, timeout=None, selfHeal=None, use_tor=None):
         if timeout is not None:
             self.timeout = timeout
         try:
             for i in tqdm(range(count)):
-                try:
-                    self.req.get(self.url, timeout=self.timeout)
-                    self.validVisits = self.validVisits + 1
-                except MissingSchema as err:
-                    self.logger.err(err)
-                    raise RuntimeError("Invalid URL")
-                except ConnectionError as err:
-                    self.logger.debug(err)
-                except ReadTimeoutError as err:
-                    self.logger.debug(err)
-                    self.timeout = self.timeout + 1
-                except ConnectTimeoutError as err:
-                    self.logger.debug(err)
-                    self.timeout = self.timeout + 1
+                if use_tor:
+                    try:
+                        socket.setdefaulttimeout(self.timeout)
+                        self.torBrowser.open(self.url)
+                        self.validVisits = self.validVisits + 1
+                        if self.timeout > 5:
+                            self.timeout = self.timeout -1
+                    except socks.GeneralProxyError:
+                        self.logger.error("TOR Proxy is Down")
+                        raise RuntimeError
+                    except socket.timeout as err:
+                        self.logger.debug(err)
+                        self.timeout = self.timeout + 1
+                    except ValueError as err:
+                        self.logger.error(err)
+                        raise RuntimeError("Invalid URL")
+                    except urllib.error.URLError as err:
+                        self.logger.debug(err)
+                        self.timeout = self.timeout + 1
+                else:
+                    try:
+                        self.req.get(self.url, timeout=self.timeout)
+                        self.validVisits = self.validVisits + 1
+                    except MissingSchema as err:
+                        self.logger.error(err)
+                        raise RuntimeError("Invalid URL")
+                    except ConnectionError as err:
+                        self.logger.debug(err)
+                    except ReadTimeout as err:
+                        self.logger.debug(err)
+                        self.timeout = self.timeout + 1
+                    except ConnectTimeoutError as err:
+                        self.logger.debug(err)
+                        self.timeout = self.timeout + 1
             if selfHeal and self.validVisits != count:
-                self.visitNoUI(count=(count - self.validVisits), timeout=self.timeout, selfHeal=False)
+                self.visitNoUI(count=(count - self.validVisits), timeout=self.timeout, selfHeal=True, use_tor=use_tor)
             self.logger.info("visited %s \t {%d} times" % (str(self.url), int(self.validVisits)))
             return self.validVisits
         except Exception as err:
